88a0c06bec6c2e8967d22ca13c4993564aa88ef1
gauravpan <gauravpandey2058@gmail.com>
2021-07-06 22:11:54 +0545

    bidding added

--- pages/api/users/profile/[id].js
+++ pages/api/users/profile/[id].js
@@ -2,16 +2,24 @@ import dbConnect from '../../../../backend/utils/dbConnect'
 import User from '../../../../backend/models/User'
 
 export default async function handler(req, res) {
-  const { query: {id}, method } = req
+  const {
+    query: { id },
+    method,
+  } = req
+  const { email } = req.query
+
   await dbConnect()
   switch (method) {
     case 'GET':
       try {
-        const users = await User.find(
-          {id}
-        ) /* find all the data in our database */
-        if(!user){
-            return res.status(404).json({ success: false, message: 'User doesn\'t exist'})
+        let user
+        if (email) user = await User.findOne({ email })
+        else
+          user = await User.findById(id) /* find all the data in our database */
+        if (!user) {
+          return res
+            .status(404)
+            .json({ success: false, message: "User doesn't exist" })
         }
         res.status(200).json({ success: true, data: user })
       } catch (error) {

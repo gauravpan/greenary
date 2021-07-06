8f381cea9a62b1d5beb005e14d82802b5fe999a1
Bibek Thapa <bibekthapa922@gmail.com>
2021-07-06 16:27:05 +0545

    Datbase callback added

--- pages/api/product/bid/index.js
+++ pages/api/product/bid/index.js
@@ -10,34 +10,42 @@ export default async function handler(req, res) {
   switch (method) {
     case 'GET':
       try {
-        const bids = await Bid.find({})
-        res.status(200).json({ success: true, data: bids })
+        Bid.find({})
+          .sort({ amount: 1 })
+          .exec((error, bids) => {
+            if (error) return res.send({ error })
+            res.status(200).json({ success: true, data: bids })
+          })
       } catch (error) {
         res.status(400).json({ success: false })
       }
       break
     case 'POST':
       try {
-        const bid = await Bid.create(
-          req.body
-        ) /* create a new model in the database */
-        console.log(bid, 'bid')
-        res.status(201).json({ success: true, data: bid })
+        Bid.create(req.body, (error, bid) => {
+          if (error) return res.send({ error })
+          res.status(201).json({ success: true, data: bid })
+        })
       } catch (error) {
         res.status(400).json({ message: 'Error Creating Bid ', error })
       }
       break
     case 'PUT':
       try {
-        const bid = await Bid.findOne({
-          productId: req.body.productId,
-        }) /* create a new model in the database */
-        if (!bid) {
-          res.status(400).json({ error: 'Bid Not found' })
-        } else {
-          bid = { ...bid, ...req.body.bid }
-          await bid.save()
-        }
+        Bid.findOne(
+          {
+            productId: req.body.productId,
+          },
+          async (error, bid) => {
+            if (error) return res.send({ error })
+            if (!bid) {
+              res.status(400).json({ error: 'Bid Not found' })
+            } else {
+              bid = { ...bid, ...req.body.bid }
+              await bid.save()
+            }
+          }
+        ) /* create a new model in the database */
       } catch (error) {
         res.status(400).json({ success: false, error })
       }
